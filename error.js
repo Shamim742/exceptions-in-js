3a7080dc23fe1b62f1ee390c0e12e798a33629d8
Shamim742 <shamimislam2742@gmail.com>
2023-02-20 08:42:20 +0600

    exceptions in js

---
+++ error.js
@@ -0,0 +1,20 @@
+// range error
+const numbers = [3, 4, 5, 8]
+numbers.length = 2;
+console.log(numbers)
+// console.log(numbers[25])
+
+// ReferenceError
+console.log(money)
+const money = 30;
+
+// syntax error
+for (let i = 0; i < 5 i++){
+    console.log(i)
+}
+
+// TypeError
+
+let student = {address: 'gazipur'};
+
+console.log(student.address.city.name)
