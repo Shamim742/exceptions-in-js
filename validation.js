ef4430c1b90a5d1b16c0a267d8f9101d19191285
Shamim742 <shamimislam2742@gmail.com>
2023-02-20 08:56:47 +0600

    validation js

---
+++ validation.js
@@ -0,0 +1,13 @@
+const number = 23;
+if (typeof number === 'number'){
+    console.log('value is number')
+}
+else{
+    console.log('value is not a number')
+}
+
+const numbers = [3, 34, 23, 32];
+// console.log(Array.isArray(numbers))
+const student = {name:'Shamim', job: 'Ghum'}
+// console.log(typeof number)
+console.log(typeof student)
